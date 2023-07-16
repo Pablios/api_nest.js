d95897ece35eb40af98c7dfcac5111ba737bb973
Pablo Guilherme <99377068+Pablios@users.noreply.github.com>
2023-07-16 19:28:30 -0300

    Validação de e-mail

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+
 @Injectable()
 export class UsuarioRepository {
   private usuarios = [];
@@ -9,5 +10,13 @@ export class UsuarioRepository {
 
   async listar() {
     return this.usuarios;
+  }
+
+  async existeComEmail(email: string) {
+    const possivelUsuario = this.usuarios.find(
+      usuario => usuario.email == email
+    );
+
+    return possivelUsuario !== undefined;
   }
 }
